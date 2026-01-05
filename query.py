1ea5d619bda5f0634db51997891c068a0466bfcf
MohammadAminSohele <mohammadaminsohele@gmail.com>
2026-01-05 08:14:33 +0330

    Update

--- query.py
+++ query.py
@@ -9,35 +9,43 @@ def connect_to_db():
     cursor = conn.cursor()
     return conn, cursor
 
-# Insert new question
-def add_question(question, option1, option2, option3, option4, correct_option):
-    conn, cursor = connect_to_db()
-    cursor.execute('''
-        INSERT INTO questions (question, option1, option2, option3, option4, correct_option)
-        VALUES (%s, %s, %s, %s, %s, %s)
-    ''', (question, option1, option2, option3, option4, correct_option))
+def insert_user(user_id, username):
+    conn ,cursor = connect()
+
+    cursor.execute("INSERT OR IGNORE INTO users (user_id, username) VALUES (%s, %s)", (user_id, username))
+
     conn.commit()
     conn.close()
 
-# Get all questions
-def get_all_questions():
-    conn, cursor = connect_to_db()
-    cursor.execute("SELECT * FROM questions")
-    questions = cursor.fetchall()
-    conn.close()
-    return questions
+# # Insert new question
+# def add_question(question, option1, option2, option3, option4, correct_option):
+#     conn, cursor = connect_to_db()
+#     cursor.execute('''
+#         INSERT INTO questions (question, option1, option2, option3, option4, correct_option)
+#         VALUES (%s, %s, %s, %s, %s, %s)
+#     ''', (question, option1, option2, option3, option4, correct_option))
+#     conn.commit()
+#     conn.close()
 
-# Save user result
-def save_result(user_id, score):
-    conn, cursor = connect_to_db()
-    cursor.execute("INSERT INTO results (user_id, score) VALUES (%s, %s)", (user_id, score))
-    conn.commit()
-    conn.close()
+# # Get all questions
+# def get_all_questions():
+#     conn, cursor = connect_to_db()
+#     cursor.execute("SELECT * FROM questions")
+#     questions = cursor.fetchall()
+#     conn.close()
+#     return questions
 
-# Get user results
-def get_user_results(user_id):
-    conn, cursor = connect_to_db()
-    cursor.execute("SELECT score FROM results WHERE user_id = %s", (user_id,))
-    scores = cursor.fetchall()
-    conn.close()
-    return scores
+# # Save user result
+# def save_result(user_id, score):
+#     conn, cursor = connect_to_db()
+#     cursor.execute("INSERT INTO results (user_id, score) VALUES (%s, %s)", (user_id, score))
+#     conn.commit()
+#     conn.close()
+
+# # Get user results
+# def get_user_results(user_id):
+#     conn, cursor = connect_to_db()
+#     cursor.execute("SELECT score FROM results WHERE user_id = %s", (user_id,))
+#     scores = cursor.fetchall()
+#     conn.close()
+#     return scores
--- schema.py
+++ schema.py
@@ -14,25 +14,12 @@ def create_tables():
     conn = psycopg2.connect(db_url)
     cur = conn.cursor()
 
-    cur.execute('''
-        CREATE TABLE IF NOT EXISTS questions (
-            id SERIAL PRIMARY KEY ,
-            question TEXT NOT NULL,
-            option1 TEXT NOT NULL,
-            option2 TEXT NOT NULL,
-            option3 TEXT NOT NULL,
-            option4 TEXT NOT NULL,
-            correct_option INTEGER NOT NULL
-        )
-    ''')
-
-    cur.execute('''
-        CREATE TABLE IF NOT EXISTS results (
-            id SERIAL PRIMARY KEY,
-            user_id BIGINT NOT NULL,
-            score INTEGER NOT NULL
-        )
-    ''')
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id TEXT PRIMARY KEY,
+        username TEXT
+    )
+    """)
 
     conn.commit()
     conn.close()
